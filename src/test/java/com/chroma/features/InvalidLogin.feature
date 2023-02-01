aa34a64b22474a615e6863f3098a969c7235552a
Samuel Coreas <samuelcoreas@macbook-pro.myfiosgateway.com>
2023-02-01 14:26:45 -0500

    invalid credentials login tested

---
+++ src/test/java/com/chroma/features/InvalidLogin.feature
@@ -0,0 +1,14 @@
+Feature: Invalid Credentials Login Test Scenario
+Description: Testing Invaild Credentials Login For Security Purposes
+
+  @Progression
+  Scenario Outline: Invalid Credentials Login
+    Given a user is on the login page "https://chroma.mexil.it/site/login"
+    When user logs in with invaild credentials username "<USERNAME>" and password "<PASSWORD>"
+    Then user sees invalid message "Invalid Username or Password"
+
+    Examples: 
+      | USERNAME            | PASSWORD |
+      | generel@teahcer.com |   123467 |
+      | general@tehcer.com  |   123678 |
+      | general@teahcer.com |   123457 |
---
+++ src/test/java/com/chroma/stepDefinitions/InvaildLogin.java
@@ -0,0 +1,34 @@
+package com.chroma.stepDefinitions;
+
+import org.testng.asserts.SoftAssert;
+
+import com.chroma.appsCommon.PageInitializer;
+import com.chroma.pages.LoginPage;
+import com.chroma.web.WebDriverUtils;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class InvaildLogin extends PageInitializer {
+    LoginPage loginPage = new LoginPage();
+
+    @Given("a user is on the login page {string}")
+    public void a_user_is_on_the_login_page(String url) {
+        WebDriverUtils.driver.get(url);
+    }
+
+    @When("user logs in with invaild credentials username {string} and password {string}")
+    public void user_logs_in_with_invaild_credentials_username_and_password(String userName, String password) {
+        loginPage.userNameTextBox.sendKeys(userName);
+        loginPage.passwordTextBox.sendKeys(password);
+        loginPage.signInBtn.click();
+    }
+
+    @Then("user sees invalid message {string}")
+    public void user_sees_invalid_message(String invalidUsernameOrPassword) {
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertEquals(loginPage.invaildLoginMessage, invalidUsernameOrPassword);
+    }
+
+}
