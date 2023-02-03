c0581d902831c35272b74c5be5df1f4775f254de
Samuel Coreas <samuelcoreas@macbook-pro.myfiosgateway.com>
2023-02-02 20:19:08 -0500

    merging

---
+++ src/test/java/com/chroma/stepDefinitions/RealEstateDashboardTextAndTabs.java
@@ -0,0 +1,24 @@
+package com.chroma.stepDefinitions;
+
+import com.chroma.appsCommon.PageInitializer;
+import com.chroma.pages.RealEstateLoginPage;
+import com.chroma.web.WebDriverUtils;
+
+import cucumber.api.java.en.Given;
+
+public class RealEstateDashboardTextAndTabs extends PageInitializer {
+    RealEstateLoginPage realEstateLoginPage = new RealEstateLoginPage();
+
+    @Given("a real estate admin is on the login page {string}")
+    public void a_real_estate_admin_is_on_the_login_page(String url) {
+        WebDriverUtils.driver.get(url);
+    }
+
+    @Given("admin logs in with valid credentials email {string} and password {string}")
+    public void admin_logs_in_with_valid_credentials_email_and_password(String email, String password) throws InterruptedException {
+        realEstateLoginPage.emailUserTextBox.sendKeys(email);
+        realEstateLoginPage.passwordUserTextBox.sendKeys(password);
+        realEstateLoginPage.loginButton.click();
+        Thread.sleep(2000);
+    }
+}
