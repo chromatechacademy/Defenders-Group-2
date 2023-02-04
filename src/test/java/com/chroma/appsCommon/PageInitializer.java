ba06e25bf793612ce74f091d3d37aaa2906e6e82
Natalya7653 <natalyakray@gmail.com>
2023-02-03 21:05:42 -0500

    Merge branch 'main' of https://github.com/chromatechacademy/Defenders-Group-2 into Natalya

--- src/test/java/com/chroma/appsCommon/PageInitializer.java
+++ src/test/java/com/chroma/appsCommon/PageInitializer.java
@@ -1,25 +1,15 @@
 package com.chroma.appsCommon;
-<<<<<<< HEAD
 
 import com.chroma.pages.CTSMSNavigationModulesPage;
-=======
 import com.chroma.pages.AcademyDashBoardPage;
 import com.chroma.pages.DashboardPage;
 import com.chroma.pages.LoginPage;
 import com.chroma.pages.RealEstateLoginPage;
 import com.chroma.pages.SamplePage;
->>>>>>> f9d8bed67a9dc7b1abe40b7995e10d8f8443ae99
 import com.chroma.web.WebDriverUtils;
 
 public class PageInitializer extends WebDriverUtils {
 
-<<<<<<< HEAD
-    public static CTSMSNavigationModulesPage cTSMSNavigationModulesPage;
-
-    public void initializeAllPages() {
-
-        cTSMSNavigationModulesPage = new CTSMSNavigationModulesPage();
-=======
     // Sample of instantiating an instance
 
     public static SamplePage samplePage;
@@ -27,6 +17,7 @@ public class PageInitializer extends WebDriverUtils {
     public static DashboardPage dashboardPage;
     public static LoginPage loginPage;
     public static RealEstateLoginPage realEstateLoginPage;
+    public static CTSMSNavigationModulesPage cTSMSNavigationModulesPage;
 
     public void initializeAllPages() {
 
@@ -35,8 +26,7 @@ public class PageInitializer extends WebDriverUtils {
         dashboardPage = new DashboardPage();
         loginPage = new LoginPage();
         realEstateLoginPage = new RealEstateLoginPage();
->>>>>>> f9d8bed67a9dc7b1abe40b7995e10d8f8443ae99
-
+        cTSMSNavigationModulesPage = new CTSMSNavigationModulesPage();
     }
 
 }
