23bbb8a45c95c71cf16dcd02a0b7ac62c8819e6f
Toropovv <iliya.toropov@gmail.com>
2023-02-06 18:30:31 -0500

    ..

--- src/test/java/com/chroma/pages/SectionsAndClassesPage.java
+++ src/test/java/com/chroma/pages/SectionsAndClassesPage.java
@@ -10,48 +10,63 @@ public class SectionsAndClassesPage {
     /* FIRST CLASS */
     @FindBy(xpath = "//td[normalize-space()='Cyber Security']")
     public WebElement CyberSecurityClass;
+
     /* SECTION IN FIRST CLASS */
     @FindBy(xpath = "//div[contains(text(),'Penetration Testing/Ethical Hacking')]")
     public WebElement penetrationTestingSection;
+
     /* SECTION IN FIRST CLASS */
     @FindBy(xpath = "//div[contains(text(),'Networking Fundamentals')]")
     public WebElement networkingFundamentalsSection;
+
     /* SECTION IN FIRST CLASS */
     @FindBy(xpath = "//div[contains(text(),'Linux Fundamentals')]")
     public WebElement linuxFundamentalsSection;
+
     /* SECTION IN FIRST CLASS */
     @FindBy(xpath = "//div[contains(text(),'CIA Triad')]")
     public WebElement ciaTriadSection;
+
     /* SECOND CLASS */
     @FindBy(xpath = "//td[normalize-space()='SDET']")
     public WebElement sdetModuleSection;
+
     /* SECTION IN SECOND CLASS */
     @FindBy(xpath = "//div[contains(text(),'Database Testing')]")
     public WebElement databaseTestingSection;
+
     /* SECTION IN SECOND CLASS */
     @FindBy(xpath = "//div[contains(text(),'Testing Fundamentals')]")
     public WebElement testingFundamentalsSection;
+
     /* SECTION IN SECOND CLASS */
     @FindBy(xpath = "//div[contains(text(),'SDLC Methodologies')]")
     public WebElement sdlcMethodologiesSection;
+
     /* SECTION IN SECOND CLASS */
     @FindBy(xpath = "//div[contains(text(),'Selenium Test Automation')]")
     public WebElement seleniumTestAutomationSection;
+
     /* SECTION IN SECOND CLASS */
     @FindBy(xpath = "//div[contains(text(),'Cucumber Fundamentals')]")
     public WebElement cucumberFundamentalsSection;
+
     /* SECTION IN SECOND CLASS */
     @FindBy(xpath = "//div[contains(text(),'API Testing')]")
     public WebElement apiTestinSection;
+
     /* SECTION IN SECOND CLASS */
     @FindBy(xpath = "//div[contains(text(),'Git/GitHub')]")
     public WebElement githubSection;
+
     /* SECTION IN SECOND CLASS */
     @FindBy(xpath = "//div[contains(text(),'Java 11 for Cool People')]")
     public WebElement javaForCoolPeopleSection;
+
     /* SECTION IN SECOND CLASS */
     @FindBy(xpath = "//div[contains(text(),'Mobile Test Automation')]")
     public WebElement mobileTestAutomationSection;
+    
     /* SECTION IN SECOND CLASS */
     @FindBy(xpath = "//div[contains(text(),'Accessibility Testing')]")
     public WebElement accessibilityTestingSection;
--- src/test/java/com/chroma/stepDefinitions/RealEstateDeleteAgentStepDef.java
+++ src/test/java/com/chroma/stepDefinitions/RealEstateDeleteAgentStepDef.java
@@ -25,7 +25,6 @@ public class RealEstateDeleteAgentStepDef extends PageInitializer {
     @Then("admin user is able to create a new agent by adding first name {string}, last name {string}, email {string}, contact number {string}, password {string}")
     public void admin_user_is_able_to_create_a_new_agent_by_adding_first_name_last_name_email_contact_number_password(
             String fname, String lname, String email, String number, String password) {
-
         realEstateNewAgentWindowPage.firstNameTextBox.sendKeys(fname);
         realEstateNewAgentWindowPage.lastNameTextBox.sendKeys(lname);
         realEstateNewAgentWindowPage.emailTextBox.sendKeys(email);
@@ -37,13 +36,11 @@ public class RealEstateDeleteAgentStepDef extends PageInitializer {
         CucumberLogUtils.logScreenShot();
         CommonUtils.sleep(3000);
         realEstateNewAgentWindowPage.backButton.click();
-
     }
 
     @Then("admin user is able to verify that the agent is listed as active with the exact first name {string},last name {string}, mail \\(email) {string}, and contact number {string}")
     public void admin_user_is_able_to_verify_that_the_agent_is_listed_as_active_with_the_exact_first_name_last_name_mail_email_and_contact_number(
             String fname, String lname, String email, String contactNumber) {
-
         realEstateDashboardPage.activeList.click();
         CommonUtils.sleep(2000);
         JavascriptUtils.scrollIntoView(RealEstateActiveAgentsPage.emailLocator(email));
@@ -51,11 +48,11 @@ public class RealEstateDeleteAgentStepDef extends PageInitializer {
         CommonUtils.assertEquals(email, actualEmail);
         String actualContactNumber = RealEstateActiveAgentsPage.contactNumberLocator(contactNumber).getText();
         CommonUtils.assertEquals(contactNumber, actualContactNumber);
-
         String fullName = fname + " " + lname;
         String actualName = RealEstateActiveAgentsPage.nameLocator(actualContactNumber).getText();
         CommonUtils.assertEquals(fullName, actualName);
-
+        CucumberLogUtils.logExtentScreenshot();
+        CucumberLogUtils.logScreenShot();
     }
 
     @When("navigates to Active Members list and delete agent from active Members list with current email {string}")
@@ -68,20 +65,17 @@ public class RealEstateDeleteAgentStepDef extends PageInitializer {
         CucumberLogUtils.logExtentScreenshot();
         CucumberLogUtils.logScreenShot();
         CommonUtils.sleep(3000);
-
     }
 
     @Then("navigates to Inactive Members list to delete user permanently with current email {string} and verify that with message {string}")
     public void navigates_to_Inactive_Members_list_to_delete_user_permanently_with_current_email_and_verify_that_with_message(
             String email, String deleteSuccess) {
-
         realEstateDashboardPage.inactiveList.click();
         JavascriptUtils.scrollIntoView(RealEstateInactiveAgentsPage.agentMenuButton(email));
         RealEstateInactiveAgentsPage.agentMenuButton(email).click();
         CucumberLogUtils.logExtentScreenshot();
         CucumberLogUtils.logScreenShot();
         RealEstateInactiveAgentsPage.agentDeleteButton(email).click();
-
         String actualDeleteSuccess = realEstateInactiveAgentsPage.assertElement.getText();
         CommonUtils.assertEquals(deleteSuccess, actualDeleteSuccess);
     }
