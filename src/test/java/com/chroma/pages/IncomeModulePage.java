b7cf5fb1c399abdddff6b84a0671b91c3418e6c0
Ekanayake Gangoda <kushangurugamage@Kushans-iMac.local>
2023-02-07 16:38:11 -0500

    after space delete

--- src/test/java/com/chroma/pages/IncomeModulePage.java
+++ src/test/java/com/chroma/pages/IncomeModulePage.java
@@ -11,19 +11,20 @@ public class IncomeModulePage {
     /* COMMON XPATH FOR INCOME MODULE */
     @FindBy(xpath = "/html/body/div[1]/aside/div/section/ul[2]/li")
     public WebElement IncomeModulelinks;
+
     /* ADD INCOME MODULE */
     @FindBy(xpath = "//a[normalize-space()='Add Income']")
     public WebElement AddIncomeModule;
+
     /*SEARCH INCOME MODULE */
     @FindBy(xpath = "//a[normalize-space()='Search Income']")
     public WebElement SearchIncomeModule;
+    
     /* INCOME HEAD MODULE */
     @FindBy(xpath = "//a[normalize-space()='Income Head']")
     public WebElement IncomeHeadModule;
 
-
     public IncomeModulePage(){
         PageFactory.initElements(WebDriverUtils.driver, this);
-    }
-    
+    } 
 }
